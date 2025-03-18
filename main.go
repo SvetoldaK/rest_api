f0e4c7ad77adae990e19358de4e8bb449808b620
Svetlana Kurilova <svetakurilova2001@mail.ru>
2025-03-18 14:17:05 +0300

    test rest-api

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
+}
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	http.ListenAndServe(":8080", router)
+}
