7069f6bf5fc751ced68ae755ed7e3b7278a64e3d
Svetlana Kurilova <svetakurilova2001@mail.ru>
2025-04-05 18:16:15 +0300

    adding makefile

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,13 +4,14 @@ import (
 	"awesomeProject/internal/database"
 	"awesomeProject/internal/handlers"
 	taskService2 "awesomeProject/internal/taskService"
-	"github.com/gorilla/mux"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	database.InitDB()
-	database.DB.AutoMigrate(&taskService2.Task{})
+	//database.DB.AutoMigrate(&taskService2.Task{})
 
 	repo := taskService2.NewTaskRepository(database.DB)
 	service := taskService2.NewService(repo)
@@ -18,10 +19,10 @@ func main() {
 	handler := handlers.NewHandler(service)
 
 	router := mux.NewRouter()
-	router.HandleFunc("/api/get", handler.GetTasksHandler).Methods("GET")
-	router.HandleFunc("/api/post", handler.PostTaskHandler).Methods("POST")
-	router.HandleFunc("/api/update/{id}", handler.UpdateTaskHandler).Methods("PATCH")
-	router.HandleFunc("/api/delete/{id}", handler.DeleteTaskHandler).Methods("DELETE")
+	router.HandleFunc("/api/tasks", handler.GetTasksHandler).Methods("GET")
+	router.HandleFunc("/api/tasks", handler.PostTaskHandler).Methods("POST")
+	router.HandleFunc("/api/tasks/{id}", handler.UpdateTaskHandler).Methods("PATCH")
+	router.HandleFunc("/api/tasks/{id}", handler.DeleteTaskHandler).Methods("DELETE")
 
 	http.ListenAndServe(":8080", router)
 }
